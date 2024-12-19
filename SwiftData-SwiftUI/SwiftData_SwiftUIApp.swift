45e9a5cad6c527308f6b79692e0c4a9b9dbdd46d
Bipul Islam <bipulhstu@gmail.com>
2024-12-19 14:55:36 +0600

    Initial Commit

---
+++ SwiftData-SwiftUI/SwiftData_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftData_SwiftUIApp.swift
+//  SwiftData-SwiftUI
+//
+//  Created by Bipul Islam on 19/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftData_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
